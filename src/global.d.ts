83a619e416425c27ae0ff604f855e97ac8ad9974
Alibek Dzhukaev <alibek.corvus@gmail.com>
2025-04-12 15:40:10 +0300

    feat: migrate to webpack

---
+++ src/global.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="webpack-env" />
+
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export default content;
+}
+
+declare module '*.css' {
+    const content: { [className: string]: string };
+    export default content;
+}
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.webp';
